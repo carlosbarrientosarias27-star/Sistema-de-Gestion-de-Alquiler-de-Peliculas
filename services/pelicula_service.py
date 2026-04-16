359140a9e8945a504db308ed007ba0f78986dd02
carlosbarrientosarias27-star <carlos.barrientosarias27@gmail.com>
2026-04-16 16:52:44 +0200

    camabios peliculas_service los id

--- services/pelicula_service.py
+++ services/pelicula_service.py
@@ -3,17 +3,17 @@ from models.pelicula import Pelicula
 from database.connection import obtener_conexion
 
 class PeliculaService:
-    def registrar_pelicula(self, id: int, titulo: str, director: str, copias: int) -> None:
+    def registrar_pelicula(self, id_pelicula: int, titulo: str, director: str, copias: int) -> None:
         if copias < 0:
             raise ValueError("Las copias no pueden ser negativas.")
         if self.buscar_por_codigo(id):
-            raise ValueError(f"Ya existe una película con el código {id}.")
+            raise ValueError(f"Ya existe una película con el código {id_pelicula}.")
         
         conn = obtener_conexion()
         cursor = conn.cursor()
         cursor.execute(
-            "INSERT INTO peliculas (id, titulo, director, copias_disponibles) VALUES (?,?,?,?)",
-            (id, titulo, director, copias)
+            "INSERT INTO peliculas (id_pelicula, titulo, director, copias_disponibles) VALUES (?,?,?,?)",
+            (id_pelicula, titulo, director, copias)
         )
         conn.commit()
         conn.close()
@@ -21,7 +21,7 @@ class PeliculaService:
     def buscar_por_codigo(self, id: int) -> Optional[Pelicula]:
         conn = obtener_conexion()
         cursor = conn.cursor()
-        cursor.execute("SELECT id, titulo, director, copias_disponibles FROM peliculas WHERE codigo = ?", (id,))
+        cursor.execute("SELECT id_pelicula, titulo, director, copias_disponibles FROM peliculas WHERE id_pelicula = ?", (id,))
         f = cursor.fetchone()
         conn.close()
         return Pelicula(f[0], f[1], f[2], f[3]) if f else None
@@ -29,7 +29,7 @@ class PeliculaService:
     def listar_peliculas(self) -> list[Pelicula]:
         conn = obtener_conexion()
         cursor = conn.cursor()
-        cursor.execute("SELECT id, titulo, director, copias_disponibles FROM peliculas")
+        cursor.execute("SELECT id_pelicula, titulo, director, copias_disponibles FROM peliculas")
         filas = cursor.fetchall()
         conn.close()
         return [Pelicula(f[0], f[1], f[2], f[3]) for f in filas]
